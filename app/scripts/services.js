ae39cdc45547101b48833fd1feb135490f98d743
juan-sanchez <juan-phidias@laptop>
2016-08-15 11:31:14 -0500

    changed method to call the API.

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -3,7 +3,7 @@
 angular.module('themoviedbApp')
 	.constant("baseURL","https://themoviedb-client.herokuapp.com/")
     .service('searchFactory', ['$http', 'baseURL', function($http,baseURL) {
-        this.searchAll = function (query,page ) {
-            return $http.get(baseURL+"search/"+query+"/page/"+page);
+    	this.searchAll = function (query,page ) {
+            return $http.jsonp(baseURL+"search/"+query+"/page/"+page+"&callback=JSON_CALLBACK");
         };             
     }]);
