c4e613084f5418250aef8c4e0c19a56c6a767ac9
juan-sanchez <juan-phidias@laptop>
2016-08-24 16:13:35 -0500

    Added validation to check the correct ID for person and movie in the URL

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -79,37 +79,50 @@ angular.module('themoviedbApp')
     }])
     // MovieController requests to the server all the information about a specific movie.
     .controller('MovieController', ['$scope', '$stateParams','searchingService', function($scope,$stateParams,  searchingService) {
-        
         $scope.movie = {};
         $scope.showMovie = false;
+        $scope.hasError = false;
         $scope.message="Loading ...";
-        searchingService.getMovie(parseInt($stateParams.id))
-            .then(
-                function(response){
-                    $scope.movie = response.data;
-                    $scope.showMovie=true;
-                },
-                function(response) {
-                    $scope.message = "Error: "+response.status + " " + response.statusText;
-                }
-            );      
+        if(!isNaN($stateParams.id)){
+            searchingService.getMovie(parseInt($stateParams.id))
+                .then(
+                    function(response){
+                        $scope.movie = response.data;
+                        $scope.showMovie=true;
+                    },
+                    function(response) {
+                        $scope.message = "Error: "+response.status + " " + response.statusText;
+                        $scope.hasError = true;
+                    }
+                );      
+        } else {
+            $scope.message = " The movie ID is not valid";
+            $scope.hasError = true;
+        }
     }])
     // PersonController requests to the server all the information about a specific person.
     .controller('PersonController', ['$scope', '$stateParams','searchingService', function($scope,$stateParams,  searchingService) {
         
         $scope.person = {};
         $scope.showPerson = false;
+        $scope.hasError = false;
         $scope.message="Loading ...";
-        searchingService.getPerson(parseInt($stateParams.id))
-            .then(
-                function(response){
-                    $scope.person = response.data;
-                    $scope.showPerson=true;
-                },
-                function(response) {
-                    $scope.message = "Error: "+response.status + " " + response.statusText;
-                }
-            );      
+        if(!isNaN($stateParams.id)){
+            searchingService.getPerson(parseInt($stateParams.id))
+                .then(
+                    function(response){
+                        $scope.person = response.data;
+                        $scope.showPerson=true;
+                    },
+                    function(response) {
+                        $scope.message = "Error: "+response.status + " " + response.statusText;
+                        $scope.hasError = true;
+                    }
+                );   
+        } else {
+            $scope.message=" The person ID is not valid";
+            $scope.hasError = true;
+        }   
     }])
     // ActorMoviesController requests to the server all the movies for a specific actor. 
     .controller('ActorMoviesController', ['$scope', '$stateParams','searchingService', function($scope,$stateParams,  searchingService) {
@@ -117,16 +130,20 @@ angular.module('themoviedbApp')
         $scope.showMovies = false;
         $scope.message ="Loading ...";
         $scope.getMoreMovies = function(page) {
-            searchingService.getMoviesByActor(parseInt($scope.person.id),page)
-                .then(
-                    function(response){
-                        $scope.movies = response.data;
-                        $scope.showMovies=true;
-                    },
-                    function(response) {
-                        $scope.message = "Error: "+response.status + " " + response.statusText;
-                    }
-                );   
+            if(!isNaN($scope.person.id)){
+                searchingService.getMoviesByActor(parseInt($scope.person.id),page)
+                    .then(
+                        function(response){
+                            $scope.movies = response.data;
+                            $scope.showMovies=true;
+                        },
+                        function(response) {
+                            $scope.message = "Error: "+response.status + " " + response.statusText;
+                        }
+                    );   
+            } else {
+                $scope.message=" The actor ID is not valid";
+            } 
         };  
         $scope.getMoreMovies(1); 
     }])
